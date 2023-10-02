e71edfd86f21184648306aad9401634e12ac497d
amanecer <amanecer.luz.cabrera@gmail.com>
2023-10-01 22:19:37 -0300

    added functions

--- text-to-speech-test.py
+++ text-to-speech-test.py
@@ -2,25 +2,28 @@ import http.client
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def textToSpeech():
+    load_dotenv()
 
-API_KEY = os.getenv("RAPIDAPI_KEY")
-conn = http.client.HTTPSConnection("text-to-speech-for-28-languages.p.rapidapi.com")
+    API_KEY = os.getenv("RAPIDAPI_KEY")
+    conn = http.client.HTTPSConnection("text-to-speech-for-28-languages.p.rapidapi.com")
 
-payload = "msg=el diego el mas grande gato, el gran barrilete cosmico"
+    payload = "msg=el diego el mas grande gato, el gran barrilete cosmico"
 
-headers = {
-    'content-type': "application/x-www-form-urlencoded",
-    'X-RapidAPI-Key': API_KEY,
-    'X-RapidAPI-Host': "text-to-speech-for-28-languages.p.rapidapi.com"
-}
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': "text-to-speech-for-28-languages.p.rapidapi.com"
+    }
 
-conn.request("POST", "/", payload, headers)
+    conn.request("POST", "/", payload, headers)
 
-res = conn.getresponse()
-data = res.read()
-with open('audio.mp3', 'wb') as audio_file:
-    audio_file.write(data)
+    res = conn.getresponse()
+    data = res.read()
+    with open('audio.mp3', 'wb') as audio_file:
+        audio_file.write(data)
 
-print("Audio file downloaded successfully.")
-print(data.decode("utf-8"))
+    print("Audio file downloaded successfully.")
+    print(data.decode("utf-8"))
+    
+textToSpeech()
--- text-to-speech.py
+++ text-to-speech.py
@@ -3,25 +3,28 @@ import json
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def apiTextToSpeech():
+    load_dotenv()
 
-url = "https://ttsmp3.com/makemp3_new.php"
-API_KEY = os.getenv("RAPIDAPI_KEY")
+    url = "https://ttsmp3.com/makemp3_new.php"
+    API_KEY = os.getenv("RAPIDAPI_KEY")
 
-data = {
-    'msg': 'el diego el mas grande gato, el gran barrilete cosmico',
-    'lang': 'Mia',
-    'source': 'ttsmp3',
-    'audioFormat': 'mp3',
-    'tasktype': 'direct',
-}
+    data = {
+        'msg': 'el diego el mas grande gato, el gran barrilete cosmico',
+        'lang': 'Mia',
+        'source': 'ttsmp3',
+        'audioFormat': 'mp3',
+        'tasktype': 'direct',
+    }
 
-headers = {
-    'content-type': "application/x-www-form-urlencoded",
-    'X-RapidAPI-Key': API_KEY,
-    'X-RapidAPI-Host': "text-to-speech-for-28-languages.p.rapidapi.com"
-}
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': "text-to-speech-for-28-languages.p.rapidapi.com"
+    }
 
-response = requests.post(url, data=data, headers=headers)
+    response = requests.post(url, data=data, headers=headers)
 
-print(response.json())
+    print(response.json())
+    
+apiTextToSpeech()
